d8ef19696e715e8a3542e449cd1c5e07b371a29f
yuanyue <87439247@qq.com>
2017-04-27 09:44:59 +0800

    修改 bug, 只 echo 一次

--- rocketmq/broker/rocketmq/entry.sh
+++ rocketmq/broker/rocketmq/entry.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
+if [ ! -f "lock.txt" ]; then
+    echo "PATH = $CONF"
+    echo "brokerId = $brokerId"
+    echo "brokerName = $brokerName"
+    echo "namesrvAddr = $namesrvAddr"
+    echo "brokerIP1 = $brokerIP1"
+    echo "listenPort = $listenPort"
+    echo "brokerRole=$brokerRole"
 
-echo "PATH = $CONF"
-echo "brokerId = $brokerId"
-echo "brokerName = $brokerName"
-echo "namesrvAddr = $namesrvAddr"
-echo "brokerIP1 = $brokerIP1"
-echo "listenPort = $listenPort"
-echo "brokerRole=$brokerRole"
+    echo -e "" >> $CONF
+    echo -e "brokerId=$brokerId" >> $CONF
+    echo -e "brokerName=${brokerName}" >> $CONF
+    echo -e "namesrvAddr=${namesrvAddr}" >> $CONF
+    echo -e "brokerIP1=${brokerIP1}" >> $CONF
+    echo -e "listenPort=${listenPort}" >> $CONF
+    echo -e "brokerRole=${brokerRole}" >> $CONF
 
-echo -e "" >> $CONF
-echo -e "brokerId=$brokerId" >> $CONF
-echo -e "brokerName=${brokerName}" >> $CONF
-echo -e "namesrvAddr=${namesrvAddr}" >> $CONF
-echo -e "brokerIP1=${brokerIP1}" >> $CONF
-echo -e "listenPort=${listenPort}" >> $CONF
-echo -e "brokerRole=${brokerRole}" >> $CONF
+    touch lock.txt
+fi
 
 cd ${ROCKETMQ_HOME}/bin && export JAVA_OPT=" -Duser.home=/opt/data" && sh mqbroker -c ${ROCKETMQ_HOME}/conf/broker.conf
